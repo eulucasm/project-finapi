29ca016092705ecf9614f2fd3b64b711657c8c5a
eulucasm <lucaspoletis@gmail.com>
2021-11-29 21:39:13 -0300

    Criando conta e verificando se cpf ja existe

--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
 const express = require('express');
+const {
+   v4: uuidv4
+} = require("uuid");
 
 const app = express();
+
+//middleware
+app.use(express.json());
+
+const customers = [];
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+   const {
+      cpf,
+      name
+   } = request.body;
+
+   const costumerAlreadyExists = customers.some((custumer) => custumer.cpf === cpf);
+   if (costumerAlreadyExists) {
+      return response.status(400).json({
+         error: "Costumer already exists"
+      });
+   }
+
+   customers.push({
+      cpf,
+      name,
+      id: uuidv4(),
+      statement: []
+   });
+
+   return response.status(201).send();
+
+});
 
 app.listen(3333);
